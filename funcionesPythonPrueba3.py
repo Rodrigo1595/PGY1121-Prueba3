fc3a19dd9d435f0c000fc49b6076963c931e8232
Vina del Mar <vina@duoc.acad>
2023-06-23 21:51:56 -0400

    Modulo 2 prueba

--- funcionesPythonPrueba3.py
+++ funcionesPythonPrueba3.py
@@ -1,29 +1,66 @@
 import os
-
-
+# Limpiar pantalla
 def limpiarPantalla():
     os.system('cls')
 
+#Modulo 1 Grabar Datos
 def grabarDatosUsuario():
     errorDato = False
-    while( not errorDato):
+    while(not errorDato):
         NIF = input("Ingrese su NIF : ")
-        if(len(NIF) < 8):
-            print("Porfavor ingrese un NIF válido (Mín 8 caracteres)")
+        if(NIF.find("-") == -1):
+            limpiarPantalla()
+            print("Porfavor ingrese un NIF válido (Formato : 99999999-XXX)")
         else:
-            nombreUsuario = input("Ingrese su nombre : ")
-            errorEdad = True
-            while (errorEdad):
-                edadUsuario = input("Ingrese su edad : ")
-                #Castear edad para forzar edades enteras
-                edadUsuario = int(edadUsuario)
-                if (edadUsuario <= 0 ):
-                    print("Porfavor ingrese una edad válida (Mayor a 0)")
-                else:
-                    print("El usuario ha sido grabado satisfactoriamente en el sistema . ")
-                    input("Presione una tecla para continuar")
-                    limpiarPantalla()
-                    return [NIF,nombreUsuario,edadUsuario]
+            listaNIF = NIF.split("-")
+            if(len(listaNIF[0]) < 9) :
+                print("Porfavor ingrese un NIF válido (Formato : 99999999-XXX)")
+            else:
+                errorNombre = True
+                while(errorNombre):
+                    nombreUsuario = input("Ingrese su nombre : ")
+                    if(len(nombreUsuario) < 8):
+                        print("Porfavor ingrese un nombre válido (Mín 8 caracteres)")
+                    else:
+                        errorNombre = False
+                        errorEdad = True
+                        while (errorEdad):
+                            edadUsuario = input("Ingrese su edad : ")
+                            #Castear edad para forzar edades enteras
+                            edadUsuario = int(edadUsuario)
+                            if (edadUsuario <= 0 ):
+                                limpiarPantalla()
+                                print("Porfavor ingrese una edad válida (Mayor a 0)")
+                            else:
+                                if(listaNIF[1].upper() == 'RTX' or listaNIF[1].upper() == 'XXY' or listaNIF[1].upper() == 'CCU' or listaNIF[1].upper() == '03F'):
+                                    perteneceUE = "Pertenece a la Unión Europea Española"
+                                else:
+                                    perteneceUE = "No pertenece a la Unión Europea Española"
+                                print("El usuario ha sido grabado satisfactoriamente en el sistema . ")
+                                input("Presione una tecla para continuar")
+                                limpiarPantalla()
+                                return [NIF,nombreUsuario,edadUsuario,perteneceUE]
         
+#Modulo 2 Buscar Usuario:
+def buscarUsuario(listaUsuarios):
+    listaUsuariosMemoria = listaUsuarios
+    encontrado = True
+    while(encontrado):
+        limpiarPantalla()
+        NIF = input("Ingrese NIF a buscar : ")
+        for i in range(len(listaUsuariosMemoria)):
+            if(listaUsuariosMemoria[i][0] == NIF ):
+                for j in range(len(listaUsuariosMemoria[i])):
+                    print(listaUsuariosMemoria[i][j])
+                input("Presione enter para continuar...")
+                return
+        encontrado = False
+        if (not encontrado):
+            respuesta = input("Vaya... Parece que ese usuario no está registrado en el sistema \n ¿Desea buscar nuevamente? S/N :  ")
+            if(respuesta.upper() == "N"):
+                return
+            else:
+                encontrado = True
+    
 
             
--- mainScript.py
+++ mainScript.py
@@ -3,15 +3,20 @@ lista = []
 menu = True
 while(menu):
     print("* Bienvenido al registro civil de Andalucía *")
-    respuesta = int(input("Porfavor ingrese opción a utilizar : ")) 
+    respuesta = int(input("Porfavor ingrese opción a utilizar : \n 1)Grabar usuario \n 2)Buscar Usuario \n 3)Imprimir Certificado \n 4)Salir \n Opc :  ")) 
     if respuesta == 1:
+        fp.limpiarPantalla()
         data = fp.grabarDatosUsuario()
         lista.append(data)
     elif respuesta == 2:
-        data
+        fp.limpiarPantalla()
+        data = fp.buscarUsuario(lista)
     elif respuesta == 3:
         data
     elif respuesta == 4:
         menu = False
+    #Herramienta DEV para debuggear lista
     elif respuesta == 5: 
         print(lista)   
+        input("Presionar Tecla")
+        fp.limpiarPantalla()
